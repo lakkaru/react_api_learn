0786f1fc57a18ec0d2e18130dcc887fadcaf22a4
Lakshman Karunasena <lakkaru@gmail.com>
2023-04-22 08:40:22 +0530

    feat- Loading screen and DidMount in class based

--- src/layouts/HomeClass.js
+++ src/layouts/HomeClass.js
@@ -3,20 +3,31 @@ import React, { Component } from "react";
 export default class HomeClass extends Component {
   state = {
     users: [],
+    isLoading: true,
   };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((data) => this.setState({users:data}));
+      .then((data) => this.setState({ users: data, isLoading: false }))
+      .catch(() => {
+        this.setState({ users: [], isLoading: false });
+      });
   }
 
   render() {
     return (
       <div>
-        {this.state.users.map((val, key) => {
-          return <div key={key}>{val.email}</div>;
-        })}
+        {this.state.isLoading === true ? (
+          <div>
+            <h2>Loading ....</h2>
+          </div>
+        ) : (
+            this.state.users.length>0?(
+          this.state.users.map((val, key) => {
+            return <div key={key}>{val.email}</div>;
+          })):(<div><p>No users found.</p></div>)
+        )}
       </div>
     );
   }
